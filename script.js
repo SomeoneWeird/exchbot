141064c2de8c7f45f82b96ce2d31503a4f53e513
Adam <ajbrady@internode.on.net>
2012-01-06 01:49:54 +1100

    Add Register functionality

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var irc = require('irc');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('users.sqlite');
 
 var channel = "#exchbot";
 
@@ -10,19 +12,23 @@ bot.join(channel);
 
 bot.addListener('message', function (from, to, message) { 
 
-	if(message.substring(0,1)=="$") { 
+	if(message.substring(0,1)=="$") {
+	 
 		message = message.substring(1);
 		console.log(message);
+		var cmd = message.split(' ')[0];
+
+		switch(cmd) {
+
+			case "register": register(from, to, message);
+				break;
+			case "test": intserttest();
+				break;
+
+		}
+
 	} else {
 		return;
-	}
-
-	//console.log(message);
-	//bot.say(channel, message);
-
-	switch(message) {
-		case "register": register(from, to, message);
-			break;
 	}
 
 });
@@ -32,13 +38,42 @@ function register(from, to, message) {
 	var args = message.split(" ");
 	var nick = args[1];
 	var gpg = args[2];
+	var reg = 0;
 
-	bot.say(channel, "registing " + nick + " with gpg key " + gpg);
 	
+	db.all("SELECT * FROM users WHERE nick = '" + nick + "' LIMIT 1;", function(err, rows) {
+  		
 
+  		rows.forEach(function (row) {
+    		if(row!=undefined) { return; }
+		});
+
+  	});	
+
+  	var stmt = db.prepare("INSERT INTO users VALUES (?, ?, ?, ?, ?)");
+	stmt.run(null, nick, gpg, null, null);
+
+	db.all("SELECT * FROM users WHERE nick = '" + nick + "' LIMIT 1;", function(err, rows) {
+  		
+  		rows.forEach(function (row) {
+    		if(row['nick']) { 
+	    		bot.say(channel, from + ": Successfully registered user " + nick + " with GPG key " + gpg); 
+		    } else {
+		    	bot.say(channel, from + ": There was an error registering, please try again later or contact an admin.");
+		    }
+		});
+
+  	});	
 		
 }
 
+function inserttest() {
+	
+	var stmt = db.prepare("INSERT INTO users VALUES (?, ?, ?, ?, ?);");
+  	stmt.run(0, "test", "test", "", "");
+  	stmt.finalize();
+
+}
 
 
 process.on('uncaughtException',function(error){
